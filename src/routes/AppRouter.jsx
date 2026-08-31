97431b5ec55ad173ccf4972858f0d0129677a4e5
hyungwoo <dhwmgl12@gmail.com>
2026-08-31 10:48:36 +0900

    A feat:car store 구현

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from '../pages/LoginPage'
-import SignupPage from '../pages/SignupPage'
-import ProductPage from '../pages/ProductPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import ProductPage from "../pages/ProductPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import CartPage from "../pages/CartPage";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<ProductPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/Signup' element={<SignupPage />} />
+        <Route path="/" element={<ProductPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
