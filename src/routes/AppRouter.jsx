4c9fa982bb457d0d3cc5aadd5e1ed5188ce6333a
hyungwoo <dhwmgl12@gmail.com>
2026-08-31 12:35:03 +0900

    feat:cart store 구현

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
-import ProductPage from "../pages/ProductPage";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "../pages/LoginPage";
-import SignupPage from "../pages/SignupPage";
 import CartPage from "../pages/CartPage";
 
 export default function AppRouter() {
