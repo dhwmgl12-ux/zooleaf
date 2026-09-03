745352782560a220f2d53a99c96d3f87524809d5
yurim <dev.yurim@gmail.com>
2026-09-03 15:57:04 +0900

    chore: var,Global 수정

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,44 +1,126 @@
 // src/styles/variables.js
+import styled from '@emotion/styled';
 export const theme = {
   colors: {
-    primary: "#4F7C3A",
-    secondary: "#8EAE67",
+    primary: '#2D6A4F',
+    secondary: '#8EAE67',
 
-    white: "#FFFFFF",
-    black: "#222222",
+    white: '#FFFFFF',
+    error: '#D90429',
+    hover: '#235240',
 
-    textPrimary: "#26352B",
-    textSecondary: "#777777",
+    textPrimary: '#2C3E35',
+    textSecondary: '#687C73',
 
-    background: "#F7F8F3",
-    border: "#DDDDDD",
+    background: '#FFFFFC',
+    background2: '#F7F9F6',
+    border: '#C8C9C9',
   },
 
   fontSize: {
-    xs: "12px",
-    sm: "14px",
-    md: "16px",
-    lg: "18px",
-    h3: "32px",
-    h2: "40px",
-    h1: "48px",
+    hero: {
+      size: '64px',
+      lineheight: '74px',
+      letterspacing: '-0.02%',
+    },
+    h1: {
+      size: '48px',
+      lineheight: '58px',
+      letterspacing: '-0.02%',
+    },
+    h2: {
+      size: '40px',
+      lineheight: '50px',
+      letterspacing: '-0.01%',
+    },
+    h3: {
+      size: '32px',
+      lineheight: '42px',
+      letterspacing: '-0.01%',
+    },
+    h4: {
+      size: '24px',
+      lineheight: '34px',
+    },
+    h5: {
+      size: '20px',
+      lineheight: '34px',
+    },
+    h6: {
+      size: '18px',
+      lineheight: '26px',
+    },
+    body: {
+      size: '16px',
+      lineheight: '26px',
+    },
+    bodylarge: {
+      size: '18px',
+      lineheight: '29px',
+    },
+    bodysmall: {
+      size: '14px',
+      lineheight: '22px',
+    },
+    caption: {
+      size: '12px',
+      lineheight: '18px',
+      letterspacing: '0.01%',
+    },
+    buttonlarge: {
+      size: '18px',
+      lineheight: '22px',
+    },
+    button: {
+      size: '16px',
+      lineheight: '19px',
+    },
+    label: {
+      size: '14px',
+      lineheight: '20px',
+      letterspacing: '0.01%',
+    },
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+    },
   },
 
   spacing: {
-    xs: "8px",
-    sm: "16px",
-    md: "24px",
-    lg: "40px",
-    xl: "80px",
+    4: '0.25rem',
+    8: '0.5rem',
+    10: '0.625rem',
+    12: '0.75rem',
+    14: '0.875rem',
+    16: '1rem',
+    20: '1.25rem',
+    24: '1.5rem',
+    32: '2rem',
+    40: '2.5rem',
+    48: '3rem',
+    56: '3.5rem',
+    64: '4rem',
+    80: '5rem',
+    100: '6.25rem',
+    140: '8.75rem',
+    200: '12.5rem',
   },
 
   radius: {
-    sm: "8px",
-    md: "16px",
-    lg: "24px",
+    button: '999px',
+    box: '20px',
+    image: '20px',
   },
 
   layout: {
-    maxWidth: "1440px",
+    maxWidth: '1440px',
+
+    breakpoint: {
+      tablet: '1023px',
+      mobile: '767px',
+      smallMobile: '375px',
+    },
   },
 };
