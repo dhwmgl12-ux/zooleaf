e93f7d0e59b43eab3bf379db6c7838bf26b91c1f
TiTaTo <hsb052890@gmail.com>
2026-09-04 11:04:32 +0900

    chore: test용 h1태그 삭제

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -3,7 +3,6 @@ import SignupForm from "../components/auth/SignupForm";
 export default function SignupPage() {
   return (
     <div>
-      <h1>회원가입 페이지</h1>
       <SignupForm />
     </div>
   );
