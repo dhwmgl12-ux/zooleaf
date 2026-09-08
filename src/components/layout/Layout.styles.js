9fad75e0fc3a3b6144d96cb2d0f41bcb1d3e69f0
kyoung <spare521@naver.com>
2026-09-08 10:46:08 +0900

    chore: 헤더 fixed 추가 및 관련 파일 수정

--- src/components/layout/Layout.styles.js
+++ src/components/layout/Layout.styles.js
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
 import { theme } from "../../styles/variables.js";
 
-export const MainContainer = styled.main`
+export const LayoutContainer = styled.main`
   width: 100%;
+  padding-top: ${({ $isMainPage }) => ($isMainPage ? "0" : "96px")};
   padding-bottom: ${theme.spacing[200]};
   background-color: ${({ $isMainPage }) => $isMainPage ? theme.colors.background : theme.colors.background2 };
 `
