18b1454d99be413926fee1739ec561d73fef6bc6
kyoung <spare521@naver.com>
2026-09-08 17:12:09 +0900

    feat: product detail Content

---
+++ src/components/detail/DetailContent.styles.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { theme } from "../../styles/variables";
+
+export const DetailContentContainer = styled.section`
+  width: 100%;
+
+  .detail-utils {
+    display: flex;
+    justify-content: space-evenly;
+  }
+`
--- src/components/detail/DetailLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DetailLayout() {
-  return (
-    <div>DetailLayout</div>
-  )
-}
