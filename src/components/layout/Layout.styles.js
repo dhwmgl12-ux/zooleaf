8fb42bb6e6b4d88b37b5bddff3ea3bfb2505dc9d
kyoung <spare521@naver.com>
2026-09-11 11:35:46 +0900

    chore: layout margin 변경

--- src/components/layout/Layout.styles.js
+++ src/components/layout/Layout.styles.js
@@ -3,16 +3,31 @@ import { theme } from "../../styles/variables.js";
 
 export const LayoutContainer = styled.main`
   width: 100%;
-  padding-top: ${({ $isMainPage, $hasBreadcrumb, }) => {
-    if ($isMainPage) {return "0";}
+  padding-top: ${({ $isMainPage, $hasBreadcrumb, $isAboutPage }) => {
+    if ($isMainPage) {
+      return "0";}
+    if ($isAboutPage) {
+      return `
+        calc(
+          var(--header-height, 96px) + var(--breadcrumb-height, 46px)
+        )
+      `;}
     if ($hasBreadcrumb) {
       return `
         calc(
           var(--header-height, 96px) + var(--breadcrumb-height, 46px) + 6.25rem
         )
       `;
+    } else {
+      return `
+        calc(
+          var(--header-height, 96px) + 6.25rem
+        )
+      `
     }
   }};
+
+
   padding-bottom: ${({ $noBottomPadding }) => ($noBottomPadding ? "0" : theme.spacing[200])};
   background-color: ${({ $isMainPage }) => $isMainPage ? theme.colors.background : theme.colors.background2 };
 `
