e0f386f89e4f01060542fe04d20f1bf5e26bbd46
kyoung <spare521@naver.com>
2026-09-14 00:14:10 +0900

    feat: Products Section 추가

--- src/components/main/ProductsSection.jsx
+++ src/components/main/ProductsSection.jsx
@@ -1,13 +1,25 @@
 
-import MainSectionHeader from './MainSectionHeader'
+import ProductCard from '../product/ProductCard.jsx'
+import MainSectionHeader from './MainSectionHeader.jsx'
+import { MainSectionContainer, MainSectionMoreLink, MainCardList,} from './MainSection.styles.js'
 
-export default function ProductsSection() {
+export default function ProductsSection({products = [],}) {
   return (
-    <section>
+    <MainSectionContainer className='section-products'>
       <MainSectionHeader 
         title="추천 티켓 & 패키지"
         description="나에게 딱 맞는 이용권으로 ZOOLEAF를 만나보세요."
       />
-    </section>
+
+      <MainCardList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <ProductCard product={product} />
+          </li>
+        ))}
+      </MainCardList>
+
+      <MainSectionMoreLink to="/products">더 둘러보기</MainSectionMoreLink>
+    </MainSectionContainer>
   )
 }
