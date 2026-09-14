ea0a14ee4af79ba2549a5c9d5f8d3c84d1f9c8c6
hyungwoo <dhwmgl12@gmail.com>
2026-09-14 11:00:58 +0900

    feat:장바구니 모바일 320 보완완료

--- src/pages/CartPage.styles.js
+++ src/pages/CartPage.styles.js
@@ -6,6 +6,10 @@ export const Container = styled.main`
   padding: ${theme.spacing[32]} ${theme.spacing[24]};
   background: ${theme.colors.background2};
   color: ${theme.colors.textPrimary};
+
+  @media (max-width: ${theme.layout.breakpoint.mobile}) {
+    padding-inline: 0;
+  }
 `;
 
 export const Title = styled.h2`
